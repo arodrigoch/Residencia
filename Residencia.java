8bef769a327c83f9f59844af0baf57e234188773
aetxabao <aitor.etxabarren@gmail.com>
2021-10-27 00:55:46 +0200

    sol

--- Residencia.java
+++ Residencia.java
@@ -14,28 +14,60 @@
  */
 public class Residencia
 {
-    //TODO: definir constantes
+    private final char DOBLE = 'D';
+    private final char NORMAL = 'N';
+    private final char ESPECIAL = 'E';
     
-    //TODO: declarar atributos 
+    private final double PRECIO_DOBLE = 150.0;
+    private final double PRECIO_NORMAL = 100.0;
+    private final double PRECIO_ESPECIAL = 300.0;
+    
+    private final double PRECIO_DESAYUNO = 100.0;
+    private final double PRECIO_CENA = 200.0;
+
+    private final  double IVA = 0.20; 
+    private final  double DESCUENTO_VERANO = 0.20; 
+    
+    private String nombre;
+
+    private int cantidadDobles;
+    private int cantidadNormales; 
+    private int cantidadEspeciales; 
+    
+    private double totalRecaudado;
+    
+    private int cantidadVerano; 
+    private int numeroFactura; 
+    private int pasesPiscina; 
     
     /**
      * Constructor - Inicializa adecuadamente todos los atributos
      */
     public Residencia(String unNombre)
     {
-        //TODO: metodo constructor, inicializa adecuadamente todos los atributos 
+        nombre = unNombre;
+        cantidadDobles = 0;
+        cantidadNormales = 0; 
+        cantidadEspeciales = 0; 
+        totalRecaudado = 0.0;
+        cantidadVerano = 0; 
+        numeroFactura = 1000000;
+        pasesPiscina = 0; 
     }
 
     /**
      *  Accesor para el nombre de la residencia
      */
-        //TODO: accesor para el nombre de la residencia
-
+    public String getNombre() {
+        return nombre;
+    }
+    
     /**
      *  Mutador para el nombre de la residencia
      */
-        //TODO: mutador para el nombre de la residencia
-    
+    public void setNombre(String queNombre) {
+        nombre = queNombre;
+    }
     
     /**
      *  (leer detalladamente el enunciado del examen en papel)
@@ -49,7 +81,7 @@ public class Residencia
      *  Por simplicidad consideraremos  en cada pedido  un unico mes
      *  Ej.        
      *  alquilarHabitacion('N', false, true, 2022, 3);  
-     *  significa que se alquila una "habitacion normal" sin desayuno pero con cena para marzo de 2021
+     *  significa que se alquila una "habitacion normal" sin desayuno pero con cena para marzo de 2022
      *  alquilarHabitacion('E', true, true, 2022, 7);   
      *  significa que se alquila una "habitacion especial" con desayuno y cena para julio de 2022
      *  
@@ -75,20 +107,136 @@ public class Residencia
      */
     public void alquilarHabitacion(char tipo, boolean desayuno, boolean cena, int ano, int mes)
     {
-        //TODO: variables locales alquilarHabitacion
+        double precioHabitacion = 0.0;
+        double precioDesayuno = 0.0;
+        double precioCena = 0.0;
+        double importeSinIva = 0.0;
+        double descuentoVerano = 0.0;
+        double iva = 0.0;
+        double importeConIva = 0.0;
+        int centenas = 0;
+        int sorteo = 0;
+        int habitacionesTotales = 0;
+		String ticket = "";
         
-        //TODO: impresion ticket si no es el tipo de habitacion que se puede alquilar en verano alquilarHabitacion
+        if ( ((mes==7) || (mes==8)) && (tipo!='E') )
+        {
+            ticket = 
+			"*******************************\n" + 
+            "Residencia " + nombre + "\n" + 
+            "*******************************\n" + 
+            "Año: " + ano + "\n" + 
+            "Mes: " + mes + "\n" + 
+            "Tipo de habitación: " + tipo + "\n" + 
+            "------------------------------------\n" + 
+            "NO DISPONIBLE\n" + 
+            "*******************************\n\n";
+			System.out.println(ticket);
+            return;
+        }
         
-        //TODO: codigo metodo alquilarHabitacion
+        numeroFactura++;
+        switch (tipo) {
+            case DOBLE:
+                cantidadDobles++;
+                precioHabitacion = PRECIO_DOBLE;
+                break;
+            case NORMAL:
+                cantidadNormales++;
+                precioHabitacion = PRECIO_NORMAL;
+                break;
+            case ESPECIAL:
+                cantidadEspeciales++;
+                precioHabitacion = PRECIO_ESPECIAL;
+                break;            
+        }
+        habitacionesTotales = cantidadDobles + cantidadNormales + cantidadEspeciales;
         
-        //TODO: impresion del ticket alquilarHabitacion
+        if (desayuno) precioDesayuno = PRECIO_DESAYUNO;
+        if (cena) precioCena = PRECIO_CENA;
         
-        //TODO: impresion estadisticas alquilarHabitacion
+        importeSinIva = precioHabitacion + precioDesayuno + precioCena;
+        
+        if ((mes==7) || (mes==8)) 
+        {
+            cantidadVerano++;
+            if (desayuno && cena) 
+            {
+                descuentoVerano = DESCUENTO_VERANO*importeSinIva;
+            }
+        }
+        
+        iva = (importeSinIva - descuentoVerano) * IVA;
+                
+        importeConIva = importeSinIva - descuentoVerano + iva;
+        
+        totalRecaudado += importeConIva;
+
+        centenas = ((int)importeConIva / 100)%10;
+        sorteo = (int)(Math.random()*10);
+        if (centenas == sorteo) pasesPiscina++; 
+        
+        // impresión del ticket
+		ticket = 
+		"*******************************\n" + 
+		"Residencia " + nombre + "\n" + 
+		"*******************************\n" + 
+        "Factura nº: " + numeroFactura + "\n" + 
+        "------------------------------------\n" + 
+        "Año: " + ano + "\n" + 
+        "Mes: " + mes + "\n" + 
+        "Tipo de habitación: " + tipo + "\n" + 
+        "Precio habitación: " + precioHabitacion + "\n" + 
+        "Precio desayuno: " + precioDesayuno + "\n" + 
+        "Precio cena: " + precioCena + "\n" + 
+        "Importe sin Iva: " + importeSinIva + "\n";
+        if (descuentoVerano > 0)
+		{
+			ticket += "Descuento verano: " + descuentoVerano + "\n";
+		}
+        ticket += 
+		"Iva: " + iva + "\n" + 
+        "------------------------------------\n" + 
+        "Importe con Iva: " + importeConIva + "\n" + 
+        "*******************************\n";
+        if (centenas == sorteo)         
+		{
+			ticket += "PREMIO: Pase piscina 1 día.\n\n";
+		}
+        else
+		{
+			ticket += centenas + " - " + sorteo + "\n\n";
+		}        
+        System.out.println(ticket);
+        // impresión estadísticas        
+        ticket = 
+		"Hasta el momento ...\n" + 
+        "Total recaudado: " + totalRecaudado + "\n" + 
+        "Habitaciones totales: " + habitacionesTotales + "\n" + 
+        "Alquileres verano: " + cantidadVerano + "\n" + 
+        "Pases piscina: " + pasesPiscina + "\n\n";
+        System.out.println(ticket);
     }
     
     /**
      *  Obtiene el tipo de habitacion mas alquilada ( D | E | N )
      */
-        //TODO: crear el metodo obtenerTipoHabitacionMasAlquilada() 
-    
+    public char obtenerTipoHabitacionMasAlquilada() 
+    {
+        char tipo = 'X';
+        if (cantidadDobles > cantidadNormales){
+            if (cantidadDobles > cantidadEspeciales){
+                tipo = DOBLE;
+            }else{
+                tipo = ESPECIAL;                
+            }
+        }else{
+            if (cantidadEspeciales > cantidadNormales){
+                tipo = ESPECIAL;
+            }else{
+                tipo = NORMAL;           
+            }            
+        }        
+        return tipo;
+    }
 }
